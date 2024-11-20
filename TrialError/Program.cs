642cf86ee3c3a70fb1a1312bb28afa4da2818760
Mia <mihangrahn@gmail.com>
2024-11-20 10:54:34 +0100

    Add project files.

---
+++ TrialError/Program.cs
@@ -0,0 +1,17 @@
+﻿// Inline-kommentar
+
+/*
+ Detta är en 
+ block-
+ kommentar
+ */
+
+
+
+//Console.WriteLine("Ange förnamn: ");
+
+//Console.Write("Hej");
+Console.Write("");
+
+
+Console.ReadLine();
